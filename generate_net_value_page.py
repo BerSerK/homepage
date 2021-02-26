c96726a2451338621cc5c084259fdd6f5f92c77d
Shiwei Ye <yeshiwei.math@gmail.com>
2021-02-26 18:24:17 +0800

    add sharpe

--- generate_net_value_page.py
+++ generate_net_value_page.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import sys
 import os
+import numpy as np
 
 filename = '/mnt/c/Users/abstr/Dropbox/myQuant/CTA/FundReport.xlsx'
 def make():
     df = pd.read_excel(filename, sheet_name = '样本外周度', names = ['date', 'net_value'],
                     header=1)
     df['net_value'] = [round(float(a), 3) for a in df['net_value']]
+    weeklyReturn = []
+    for i in range(1, len(df)):
+        weeklyReturn.append(df['net_value'].iloc[i]/df['net_value'][i-1] - 1)
+    sharpe = np.mean(weeklyReturn)*np.sqrt(52)/np.std(weeklyReturn)
+    print("sharpe:", sharpe)
     date_str = str(list([d.date().strftime('%Y-%m-%d') for d in df['date']]))
     data_str = str(list(df['net_value']))
     draw_down = [0]
@@ -16,7 +22,7 @@ def make():
         if net_value[i] > pre_max:
             pre_max = net_value[i]
         draw_down.append(round(-100*(1-net_value[i]/pre_max), 3))
-    df = pd.read_excel(filename, sheet_name = '自营', header = 0)
+    df = pd.read_excel(filename, sheet_name = '阶段二', header = 0)
     ytd = "%.2f%%"%(list(df['YTD'])[-1]*100)
     draw_down_str = str(draw_down)
     template = open('net_value_template.html').read()
