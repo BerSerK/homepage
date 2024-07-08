5dbd0a44a1c08867f5c1d804e8ba02eb7f83fc48
Shiwei.Ye <shiwei@yeshiwei.com>
2024-07-08 12:04:59 +0800

    调整每日任务脚本，把绝对路径改为相对路径，添加log日志。

--- run_daily.sh
+++ run_daily.sh
@@ -1,7 +1,13 @@
 #!/bin/bash  
-  
-# 切换到脚本所在目录  
-cd "$(dirname "$0")"  
+# 该脚本用于每日定时执行，生成净值页面
+# 把输出重定向到以日期命名的文件.
+exec >> /mnt/d/log/homepage/`date +%Y%m%d`.log 2>&1
+
+# 获取当前目录
+project_path=$(cd `dirname $0`; pwd)  
+
+# 切换到脚本所在目录
+cd $project_path
   
 # 执行命令  
-/home/alex/anaconda3/bin/ipython /home/alex/code/homepage/generate_net_value_page.py send
+/home/alex/anaconda3/bin/ipython $project_path/generate_net_value_page.py send
